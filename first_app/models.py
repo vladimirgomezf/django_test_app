64b5287d0148283b9526d973947f98bd1b9c1009
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-05-25 09:24:49 -0400

    first commit

---
+++ first_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Address(models.Model):
+    street = models.CharField('Street', max_length=250)
+    city = models.CharField('City', max_length=50)
+    state = models.CharField('State', max_length=50)
+    zip = models.CharField('Postal Code', max_length=6)
+    country = models.CharField('Country', max_length=50)
+
+    def __str__(self) -> str:
+        return f'{self.street}, {self.city}.'
