ed50efc83edde9a276d92678797f006a72f7fc0a
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-07-07 08:52:45 -0400

    Made some adjustments...

--- first_app/serializers.py
+++ first_app/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-from first_app.models import Address
+from .models import Address
 
 
 class AddressSerializer(serializers.HyperlinkedModelSerializer):
@@ -9,14 +9,3 @@ class AddressSerializer(serializers.HyperlinkedModelSerializer):
         model = Address
         fields = ["street", "city", "state", "zip", "country"]
 
-    # def create(self, validated_data):
-    #     return Address.objects.create(**validated_data)
-    #
-    # def update(self, instance, validated_data):
-    #     instance.street = validated_data.get('street', instance.street)
-    #     instance.city = validated_data.get('city', instance.city)
-    #     instance.state = validated_data.get('state', instance.state)
-    #     instance.zip = validated_data.get('zip', instance.zip)
-    #     instance.country = validated_data.get('country', instance.country)
-    #     instance.save()
-    #     return instance
